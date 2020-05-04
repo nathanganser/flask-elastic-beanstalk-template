45d6e3f1c4146add6d406d98e15ab4dbdf1603ac
Nathan <me@nathanganser.com>
2020-05-04 09:30:12 +0000

    message

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+    return "Hello Stefano! Here is a secure EB application :) "
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
